83b05c0d443172e7e13009f3f694325f7b27470d
Cliff Yang <cliffyyc@outlook.com>
2022-04-09 21:37:17 -0400

    release mechanic

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -2,45 +2,63 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Ball : MonoBehaviour
-{
-    public float delay = 30f;
+public class Ball : MonoBehaviour {
     private float hoopTop = -1f;
+    public bool holding = false;
+    private bool shooting = false;
+    private float shootTime;
+    private float SHOOTTIME = 1f;
 
-    private SpriteRenderer ball;
+    public GameObject hand;
+    public GameObject arm;
 
-    private void Start()
-    {
-        ball = GetComponent<SpriteRenderer>();
+    private SpriteRenderer ball_SpriteRenderer;
+    private Rigidbody2D ball_Rigidbody2D;
+    private CircleCollider2D ball_CircleCollider2D;
+
+    private void Start() {
+        ball_SpriteRenderer = GetComponent<SpriteRenderer>();
+        ball_Rigidbody2D = GetComponent<Rigidbody2D>();
+        ball_CircleCollider2D = GetComponent<CircleCollider2D>();
+
+        shootTime = SHOOTTIME;
     }
 
-    void Update()
-    {
-        delay -= Time.deltaTime;
-        if (delay <= 0)
-        {
-            Debug.Log("destroying");
-            Destroy(this.gameObject);
+    void Update() {
+        //Debug.Log(Physics.GetIgnoreLayerCollision(7, 9));
+        if (Input.GetKeyDown(KeyCode.R)) {
+            Vector3 handPos = hand.transform.position;
+            ball_Rigidbody2D.MovePosition(new Vector2(handPos.x, handPos.y));
         }
+
+        if (Input.GetMouseButtonUp(0)) {
+            holding = false;
+            shooting = true;
+        }
+
+        if (holding) {
+            transform.position = hand.transform.position;
+        }
+
+        if (this.shooting) {
+            Physics2D.IgnoreLayerCollision(7, 9);
+            shootTime -= Time.deltaTime;
+            if (shootTime <= 0) {
+                Physics2D.IgnoreLayerCollision(7, 9, false);
+                shootTime = SHOOTTIME;
+                shooting = false;
+            }
+        }
+        Debug.Log(shootTime);
     }
 
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        if (collision.CompareTag("HoopTop"))
-        {
-            hoopTop = Time.time;
+    private void OnTriggerEnter2D(Collider2D collision) {
+        if (collision.CompareTag("HoopTop")) hoopTop = Time.time;
+        if (collision.CompareTag("HoopBottom")) {
+            if (Time.time - hoopTop <= 1f) ball_SpriteRenderer.color = new Color(0, 1, 0, 1);
+            else hoopTop = -1f;
         }
 
-        if (collision.CompareTag("HoopBottom"))
-        {
-            if (Time.time - hoopTop <= 1f)
-            {
-                ball.color = new Color(0, 1, 0, 1);
-            }
-            else
-            {
-                hoopTop = -1f;
-            }
-        }
+        if (collision.CompareTag("Hand")) this.holding = true;
     }
 }
--- Assets/Scripts/Game.cs
+++ Assets/Scripts/Game.cs
@@ -2,30 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Game : MonoBehaviour
-{
-    public float ballInterval = 4f;
-    private float currentBallInterval;
+public class Game : MonoBehaviour {
     public GameObject ballPrefab;
 
-    private void Start()
-    {
-        currentBallInterval = ballInterval;
-    }
+    public GameObject ball;
+    public GameObject player;
 
     // Update is called once per frame
-    void Update()
-    {
-        spawnBall();
+    void Update() {
+
     }
 
-    void spawnBall()
-    {
-        currentBallInterval -= Time.deltaTime;
-        if (currentBallInterval <= 0)
-        {
-            Instantiate(ballPrefab, new Vector3(0,5,0), Quaternion.identity);
-            currentBallInterval = ballInterval;
-        }
-    }
 }
