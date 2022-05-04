c690650e61d5a1a8d2b7754451d399cafdd82075
Cliff Yang <cliffyyc@outlook.com>
2022-05-03 22:39:01 -0400

    re-added jump and run

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -3,79 +3,83 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Ball : MonoBehaviour {
-	// Timer to check hoop top and bottom collision order
-	private float hoopTopTimer = -1f;
-	private bool reset = false;
-	private float greenTime;
-	private readonly float GREENTIME = 1f;
-	private bool green = false;
+    // Timer to check hoop top and bottom collision order
+    private float hoopTopTimer = -1f;
+    private bool reset = false;
+    private float greenTime;
+    private readonly float GREENTIME = 1f;
+    private bool green = false;
 
-	public GameObject hand;
-	public GameObject body;
+    public GameObject hand;
+    public GameObject body;
 
-	private SpriteRenderer ball_SpriteRenderer;
-	private Rigidbody2D ball_Rigidbody2D;
-	private PlayerController player_Script;
-	private Rigidbody2D player_Rigidbody2D;
-	private Collider2D hand_Collider;
+    private SpriteRenderer ball_SpriteRenderer;
+    private Rigidbody2D ball_Rigidbody2D;
+    private PlayerController player_Script;
+    private Rigidbody2D player_Rigidbody2D;
+    private Collider2D hand_Collider;
 
-	private void Start() {
-		ball_SpriteRenderer = GetComponent<SpriteRenderer>();
-		ball_Rigidbody2D = GetComponent<Rigidbody2D>();
-		player_Script = body.GetComponent<PlayerController>();
-		player_Rigidbody2D = body.GetComponent<Rigidbody2D>();
-		hand_Collider = hand.GetComponent<Collider2D>();
-	}
+    private void Start() {
+        ball_SpriteRenderer = GetComponent<SpriteRenderer>();
+        ball_Rigidbody2D = GetComponent<Rigidbody2D>();
+        player_Script = body.GetComponent<PlayerController>();
+        player_Rigidbody2D = body.GetComponent<Rigidbody2D>();
+        hand_Collider = hand.GetComponent<Collider2D>();
+    }
 
-	void Update() {
+    void Update() {
+        if (Input.GetKeyDown(KeyCode.R)) {
+            ball_Rigidbody2D.position = new Vector2(12, 3);
+            ball_Rigidbody2D.velocity = new Vector2(0, 0);
+            ball_Rigidbody2D.angularVelocity = 0;
+        }
+        
+        if (green) {
+            // Resets ball color after enought time has passed
+            if (Time.time - greenTime > GREENTIME) {
+                ball_SpriteRenderer.color = new Color(1, 1, 1, 1);
+                green = false;
+            }
+        }
 
 
-		if (green) {
-			// Resets ball color after enought time has passed
-			if (Time.time - greenTime > GREENTIME) {
-				ball_SpriteRenderer.color = new Color(1, 1, 1, 1);
-				green = false;
-			}
-		}
-		
+        // if dribbling, constrain ball x to hand x
+        if (player_Script.dribbling) {
+            transform.position = new(hand.transform.position.x, transform.position.y);
+            // in case the ball goes above the hand let the ball pass through
+            if (transform.position.y > hand.transform.position.y) {
+                hand_Collider.isTrigger = true;
+            } else {
+                hand_Collider.isTrigger = false;
+            }
+        }
+    }
 
-		// if dribbling, constrain ball x to hand x
-		if (player_Script.dribbling) {
-			transform.position = new(hand.transform.position.x, transform.position.y);
-			// in case the ball goes above the hand let the ball pass through
-			if (transform.position.y > hand.transform.position.y) {
-				hand_Collider.isTrigger = true;
-			} else {
-				hand_Collider.isTrigger = false;
-			}
-		}
-	}
+    public void FixedUpdate() {
 
-	public void FixedUpdate() {
+    }
 
-	}
+    private void OnTriggerEnter2D(Collider2D collision) {
+        // Checks hoop collision with to triggers
+        if (collision.CompareTag("HoopTop")) hoopTopTimer = Time.time;
+        if (collision.CompareTag("HoopBottom")) {
+            if (Time.time - hoopTopTimer <= 1f) {
+                ball_SpriteRenderer.color = new Color(0, 0.75f, 0, 1);
+                green = true;
+                greenTime = Time.time;
+            } else hoopTopTimer = -1f;
+        }
 
-	private void OnTriggerEnter2D(Collider2D collision) {
-		// Checks hoop collision with to triggers
-		if (collision.CompareTag("HoopTop")) hoopTopTimer = Time.time;
-		if (collision.CompareTag("HoopBottom")) {
-			if (Time.time - hoopTopTimer <= 1f) {
-				ball_SpriteRenderer.color = new Color(0, 0.75f, 0, 1);
-				green = true;
-				greenTime = Time.time;
-			} else hoopTopTimer = -1f;
-		}
-		
 
-	}
+    }
 
-	private void OnTriggerExit2D(Collider2D collision) {
+    private void OnTriggerExit2D(Collider2D collision) {
 
-	}
+    }
 
-	private void OnCollisionEnter2D(Collision2D collision) {
-		// Activates only when the hand is not a trigger (dribbling)
-		// Gives the ball downward dribbling force
+    private void OnCollisionEnter2D(Collision2D collision) {
+        // Activates only when the hand is not a trigger (dribbling)
+        // Gives the ball downward dribbling force
 
-	}
+    }
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,95 +2,110 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
-{
-	// Player components
-	private Rigidbody2D player_Rigidbody2D;
-	private Animator player_Animator;
+public class PlayerController : MonoBehaviour {
+    // Player components
+    private Rigidbody2D player_Rigidbody2D;
+    private Animator player_Animator;
 
-	// Other GameObjects
-	public GameObject hand;
-	public GameObject ball;
-	private Rigidbody2D ball_Rigidbody2D;
-	private Collider2D hand_Collider;
+    // Other GameObjects
+    public GameObject hand;
+    public GameObject ball;
+    private Rigidbody2D ball_Rigidbody2D;
+    private Collider2D hand_Collider;
 
-	// Player parameters
-	public float runParam = 15f;
-	public float jumpParam = 20f;
-	public float shootParam = 0.13f;
-	public float dribbleParam = 0.1f;
-	public float inputX;
-	private bool mouseButton0 = false;
-	private Vector3 mouse;
-	private bool reset = false;
+    // Player parameters
+    private float runParam = 18f;
+    private float jumpParam = 18f;
+    private float shootParam = 0.13f;
+    private float dribbleParam = 0.1f;
+    private float inputX;
+    private Vector3 mouse;
 
-	// Game Logic variables
-	public bool holding = false;
-	private bool shoot = false;
-	public bool forceShoot = false;
-	private bool layup = false;
-	public bool jump = false;
-	public bool grounded = false;
-	public bool stopped = false;
-	public bool dribbling = false;
+    // Game Logic variables
+    private bool jump = false;
+    private bool grounded = false;
+    public bool dribbling = false;
+    private bool stopped = true;
 
-	private float shootTime = 0f;
-	private readonly float SHOOTTIME = 1.5f;
-	private readonly float SHOOTTIMEMIN = 0.2f;
-	private readonly float BASESHOOTTIME = 0.5f;
-	private readonly float forceShootError = 0.3f;
-	// stopped checks if the player is able to continue run around(dribblle)
-	public float rotZ;
+    // stopped checks if the player is able to continue run around(dribblle)
+    public float rotZ;
 
 
-	private void Start() {
-		player_Rigidbody2D = GetComponent<Rigidbody2D>();
-		player_Animator = GetComponent<Animator>();
-		ball_Rigidbody2D = ball.GetComponent<Rigidbody2D>();
-		hand_Collider = hand.GetComponent<Collider2D>();
-	}
+    private void Start() {
+        player_Rigidbody2D = GetComponent<Rigidbody2D>();
+        player_Animator = GetComponent<Animator>();
+        ball_Rigidbody2D = ball.GetComponent<Rigidbody2D>();
+        hand_Collider = hand.GetComponent<Collider2D>();
+    }
 
-	// Update is called once per frame
-	// Input detection should be in Update()
-	private void Update() {
-		inputX = Input.GetAxisRaw("Horizontal");
-		mouse = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+    // Update is called once per frame
+    // Input detection should be in Update()
+    private void Update() {
+        inputX = Input.GetAxisRaw("Horizontal");
+        mouse = Camera.main.ScreenToWorldPoint(Input.mousePosition);
 
-		// Calculates the mouse facing and inverts sprite if it is opposite
-		Vector3 difference = mouse - transform.position;
-		rotZ = Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg;
-		bool faceRight = Mathf.Abs(rotZ) <= 90;
+        // Calculates the mouse facing and inverts sprite if it is opposite
+        Vector3 difference = mouse - transform.position;
+        rotZ = Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg;
+        bool faceRight = Mathf.Abs(rotZ) <= 90;
 
-		Vector3 scale = transform.localScale;
-		if (faceRight != (scale.x > 0)) {
-			transform.localScale = new(scale.x * -1, scale.y, scale.z);
-		}
+        Vector3 scale = transform.localScale;
+        if (faceRight != (scale.x > 0)) {
+            transform.localScale = new(scale.x * -1, scale.y, scale.z);
+        }
 
-		if (Input.GetButton("Jump")) {
-			jump = true;
-		}
-	}
+        if (Input.GetKeyDown(KeyCode.R)) {
+            player_Rigidbody2D.position = new Vector2(15, -5);
+            player_Rigidbody2D.velocity = new Vector2(0, 0);
+
+            grounded = false;
+            jump = false;
+            dribbling = false;
+            stopped = false;
+        }
+
+        if (Input.GetButtonDown("Jump")) {
+            // cannot jump if !grounded/dribbling
+            jump = true;
+        }
+    }
 
 
-	private void FixedUpdate() {
-		// Shooting sequence (normal shot, forced shot, layup)
-		// forced shots have error scalers
-		Vector3 difference = mouse - transform.position;
-		Vector2 shootDirection = new(difference.x, difference.y);
-		shootDirection.Normalize();
+    private void FixedUpdate() {
+        // Shooting sequence (normal shot, forced shot, layup)
+        // forced shots have error scalers
+        Vector3 difference = mouse - transform.position;
+        Vector2 shootDirection = new(difference.x, difference.y);
+        shootDirection.Normalize();
+        
+        if (jump) {
+            jump = false;
+            if (grounded && !dribbling) {
+                Vector2 jumpForce = new Vector2(0, jumpParam);
+                if (stopped) {
+                    jumpForce.x = inputX * runParam / 1.5f;
+                }
+                player_Rigidbody2D.AddForce(jumpForce, ForceMode2D.Impulse);
+            }
+        }
 
-	}
+        if (inputX != 0) {
+            if (!stopped && grounded) {
+                player_Rigidbody2D.velocity = new Vector2(inputX * runParam, player_Rigidbody2D.velocity.y);
+            }
+        }
+    }
 
+    
+    private void OnCollisionEnter2D(Collision2D collision) {
+        if (collision.gameObject.CompareTag("Ground")) {
+            grounded = true;
+        }
+    }
 
-	private void OnCollisionEnter2D(Collision2D collision) {
-		if (collision.gameObject.CompareTag("Ground")) {
-			grounded = true;
-		}
-	}
-
-	private void OnCollisionExit2D(Collision2D collision) {
-		if (collision.gameObject.CompareTag("Ground")) {
-			grounded = false;
-		}
-	}
+    private void OnCollisionExit2D(Collision2D collision) {
+        if (collision.gameObject.CompareTag("Ground")) {
+            grounded = false;
+        }
+    }
 }
