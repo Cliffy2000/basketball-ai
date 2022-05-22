cc8a5a1de4c4cfa16e3b6af0a2aba2ff988d0346
Cliff Yang <cliffyyc@outlook.com>
2022-05-21 22:05:06 -0400

    ball training 2

--- Assets/Scripts/Training 2/BallTraining2.cs
+++ Assets/Scripts/Training 2/BallTraining2.cs
@@ -6,24 +6,38 @@ public class BallTraining2 : MonoBehaviour
 {
     private SpriteRenderer ball_spriteRenderer;
 
-    void Start()
-    {
+    private GameObject player;
+    private PlayerTraining2 player_script;
+
+    private float hoopTopTimer = -1f;
+
+    public void Start() {
         ball_spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
 
-    void Update()
-    {
+    public void Update() {
         
     }
 
-
-    private void OnTriggerEnter2D(Collider2D collision) {
-        
+    public void setPlayer(GameObject player) {
+        this.player = player;
+        player_script = this.player.GetComponent<PlayerTraining2>();
     }
 
 
-    private void OnCollisionEnter2D(Collision2D collision) {
-        
+    private void OnTriggerExit2D(Collider2D collision) {
+        if (collision.gameObject.name == "Eval") {
+            float exitScore = (-transform.position.x - 6.7f) * 2 + 20;
+            player_script.gene.score = Mathf.Max(player_script.gene.score, exitScore);
+        }
+
+        if (collision.CompareTag("HoopTop")) hoopTopTimer = Time.time;
+        if (collision.CompareTag("HoopBottom")) {
+            if (Time.time - hoopTopTimer <= 1f) {
+                player_script.gene.score = 100f;
+                ball_spriteRenderer.color = new Color(0, 0.75f, 0, 1);
+            } else hoopTopTimer = -1f;
+        }
     }
 }
